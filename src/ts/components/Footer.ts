a132bac0fb1a4db13eb3ac60c874c238d8707009
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2025-11-18 10:31:15 +0100

    Created a footer in Footer.ts and added styling in index.html for sticky footer and header.

---
+++ src/ts/components/Footer.ts
@@ -0,0 +1,10 @@
+export function Footer() {
+  return `
+        <div class="mx-auto flex max-w-7xl flex-col gap-4">
+        <figure>
+          <img src="/assets/images/logo-secondary-48.png" alt="Trove logo" />
+        </figure>
+        <p>Copyright © 2025 Trove</p>
+      </div>
+    `;
+}
