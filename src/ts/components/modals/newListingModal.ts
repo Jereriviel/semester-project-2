cded013d6cb23259775ad896cdff6f32d78111c3
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2025-12-05 11:32:08 +0100

    Started on New Listing Modal

--- src/ts/components/modals/newListingModal.ts
+++ src/ts/components/modals/newListingModal.ts
@@ -1,3 +1,34 @@
+import { createModal } from "../../utils/createModal.js";
+
 export function openNewListingModal() {
-  console.log("Open modal");
+  const modal = createModal(
+    `
+    <p>New Listing</p>
+    `
+  );
+
+  document.body.appendChild(modal);
+  modal.showModal();
+
+  const addImageBtn = modal.querySelector<HTMLButtonElement>("#add-image-btn")!;
+
+  addImageBtn.addEventListener("click", () => console.log("Add new image"));
+
+  const addListingBtn =
+    modal.querySelector<HTMLButtonElement>("#add-listing-btn")!;
+
+  addListingBtn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("Submit New Listing)");
+  });
+
+  const closeBtn = modal.querySelector<HTMLButtonElement>(
+    "#close-new-listing-btn"
+  )!;
+
+  closeBtn.addEventListener("click", () => modal.close());
+
+  modal.addEventListener("close", () => {
+    modal.remove();
+  });
 }
